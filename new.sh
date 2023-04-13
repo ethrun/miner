5e4a583f4e925782a9b23bdb003412eb9a1e77cf
Stark <43443357+stark-eth@users.noreply.github.com>
2023-04-13 12:44:59 +0800

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -xvzf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 cd bin
-./ethminer - -P stratum+tcp://0xECd31e195157D398B7b14ABA0B3d41C131D4375C:x@eth.2miners.com:2020
+./ethminer -C -P stratum+tcp://0xECd31e195157D398B7b14ABA0B3d41C131D4375C:x@eth.2miners.com:2020
